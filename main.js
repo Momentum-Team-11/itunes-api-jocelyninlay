b41842c405ab7aa4d820b2a71e7461289e982500
jocelyninlay <inlay.j@gmail.com>
2022-02-10 19:57:45 -0500

    brings back search with + in it

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ const form = document.querySelector('#form')
 form.addEventListener("submit", function (event){
     event.preventDefault()
     console.log("event happened!")
+    const formData = new FormData(event.target);
+   const asString = new URLSearchParams(formData).toString('');
+   console.log(asString);
 })
 
-const iTunesUrl = 'https://itunes.apple.com/search?'
+let url = URL(`https://itunes.apple.com/search?term=${asString}`)
+//     let params = URLSearchParams(url.search);
+//     params.append('','');
 
-fetch(iTunesUrl)
+fetch(url)
 .then(function(response){
     console.log(response)
     return response.json()
 })
 .then (function(data){
     console.log(data)
-
+    
+    
 })
