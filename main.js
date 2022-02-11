7416ca08d67e2692bd63dd71f836a2ab6e2c32ea
jocelyninlay <inlay.j@gmail.com>
2022-02-11 11:58:52 -0500

    list is showing

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ form.addEventListener("submit", function (event){
     event.preventDefault()
     console.log("event happened!")
     const formData = new FormData(event.target);
+    console.log(formData.keys())
    const asString = new URLSearchParams(formData).toString('');
    console.log(asString);
-  let url = (`https://itunes.apple.com/search?${asString}`)
+  let url = (`https://itunes.apple.com/search?${asString}&entity=song&limit=20`)
 fetch(url)
 .then(function(response){
     console.log(response)
@@ -15,4 +16,10 @@ fetch(url)
 .then (function(data){
     console.log(data) 
 
-})}) 
+for (let i = 0; i < data.results.length; i++) {
+    console.log('the value of i is:' , i) 
+    console.log('the artist name is' , data.results[i].artistName)  
+document.querySelector('#music').innerHTML +=`<div>${data.results[i].artistName}</div>`
+}
+})
+}) 
